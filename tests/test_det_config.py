ebad7c4472269692f5e0c31429892e59a1ae727e
jlashner <jlashner@gmail.com>
2020-04-06 21:56:13 -0700

    DetConfig with docs and tests

--- tests/test_det_config.py
+++ tests/test_det_config.py
@@ -1,18 +1,37 @@
 import pytest
 from sodetlib.det_config import DetConfig
 import os
-import shutil
 
 
-def setup_module():
-    if os.path.exists('config'):
-        shutil.rmtree('config')
+@pytest.fixture
+def cfg():
+    (sys, dev, pysmurf) = [
+        os.path.join(os.path.dirname(__file__), f'input/{fname}')
+        for fname in ['sys_config.yml', 'dev_cfg.yml', 'pysmurf.cfg']
+    ]
+    return DetConfig(sys_file=sys, dev_file=dev, pysmurf_file=pysmurf)
+
+def test_dev_cfg(cfg):
+    cfg.parse_args(args=[])
+    cfg.dev.update_band(1, {'dc_att': 3, 'drive': 11})
+    cfg.dev.update_bias_group(0, {'bias_high': 11, 'enabled': 1})
+    cfg.dev.update_experiment({"amp_50k_Id": 12})
+    run, sys, dev = cfg.dump_configs('config', clobber=True)
+
+    # Testing reloading written config file
+    cfg = DetConfig(sys_file=sys, dev_file=dev)
+    cfg.parse_args(args=[])
+    assert (cfg.dev.bands[1]['dc_att'] == 3)
 
 
+def test_failed_update(cfg):
+    cfg.parse_args(args=[])
+    with pytest.raises(ValueError):
+        cfg.dev.update_experiment({'abcd': 12})
 
 
-def test_dev_cfg():
-    cfg = DetConfig()
-    cfg.load_config_files()
-    cfg.dump_configs('config')
-    assert (True)
+def test_offline_pysmurf_instance(cfg):
+    cfg.parse_args(args=[])
+    S = cfg.make_pysmurf_instance(offline=True, dump_configs=True)
+
+
