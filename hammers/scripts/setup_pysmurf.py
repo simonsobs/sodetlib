ed8bd503e5383e575d919b53a186838535dec389
jlashner <jlashner@gmail.com>
2020-04-28 23:11:08 -0700

    Small jackhammer improvements

--- hammers/scripts/setup_pysmurf.py
+++ hammers/scripts/setup_pysmurf.py
@@ -1,29 +1,10 @@
 import matplotlib
 matplotlib.use('Agg')
-import pysmurf.client
-import argparse
-import subprocess
-import os
-import yaml
+from sodetlib.det_config import DetConfig
+
 
 if __name__ == "__main__":
-    sys_config_file = os.path.join(os.environ['OCS_CONFIG_DIR'], 'sys_config.yml')
-    with open(sys_config_file, 'r') as stream:
-        sys_config = yaml.safe_load(stream)
-
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--slot', '-N')
-    parser.add_argument('--config-file', '-c', default=sys_config['pysmurf_config_file'])
-    parser.add_argument('--shelf-manager', '-S', default=sys_config['shelf_manager'])
-
-    args = parser.parse_args()
-
-    epics_root = f'smurf_server_s{args.slot}'
-
-    S = pysmurf.client.SmurfControl(
-        epics_root=epics_root, cfg_file=args.config_file,
-        setup=True, make_logfile=False
-    )
-
+    cfg = DetConfig()
+    cfg.parse_args()
+    S = cfg.get_smurf_control(setup=True, dump_configs=True)
     S.set_stream_enable(0)
--- hammers/scripts/start_pysmurf_ipython.py
+++ hammers/scripts/start_pysmurf_ipython.py
@@ -3,39 +3,23 @@ import traitlets.config
 import argparse
 import os
 import yaml
+from sodetlib.det_config import DetConfig
+import numpy as np
+
+try:
+    import matplotlib.pyplot as plt
+except Exception:
+    print("Trouble importing Matplotlib! Using agg backend")
+    import matplotlib
+    matplotlib.use("Agg")
+    import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    sys_config_file = os.path.join(os.environ['OCS_CONFIG_DIR'], 'sys_config.yml')
-    with open(sys_config_file, 'r') as stream:
-        sys_config = yaml.safe_load(stream)
+    cfg = DetConfig()
 
     parser = argparse.ArgumentParser()
-
-    parser.add_argument('--slot', '-N', type=int)
-    parser.add_argument('--config-file', '-c', default=sys_config['pysmurf_config_file'])
-    parser.add_argument('--setup', action='store_true')
-    parser.add_argument('--make-logfile', action='store_true')
-    parser.add_argument('--agg', action='store_true')
-
-    args = parser.parse_args()
-
-    if args.agg:
-        import matplotlib
-        matplotlib.use('Agg')
-    import pysmurf.client
-
-    epics_root = f'smurf_server_s{args.slot}'
-    # Sets pysmurf publisher id
-    crate = sys_config['crate_id']
-    os.environ['SMURFPUB_ID'] = f'crate{crate}_slot{args.slot}'
-
-    print(f"Creating pysmurf object for slot {args.slot}")
-    S = pysmurf.client.SmurfControl(
-        epics_root=epics_root,
-        cfg_file=args.config_file,
-        setup=args.setup,
-        make_logfile=args.make_logfile
-    )
+    args = cfg.parse_args()
+    S = cfg.get_smurf_control(dump_configs=True)
 
     _ipython_config = traitlets.config.get_config()
     _ipython_config.InteractiveShellEmbed.colors = "LightBG"
