749017c03cb3e0b6054130ea3250fb05bf03c066
msilvafe <msilvafe@ucsd.edu>
2020-06-25 16:27:14 -0700

    Added streaming on and off scripts for use with rf testing

---
+++ scratch/max/stream_data_on.py
@@ -0,0 +1,20 @@
+import pysmurf.client
+import argparse
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--setup', action='store_true')
+    parser.add_argument('--config-file', required=True)
+    parser.add_argument('--epics-root', default='smurf_server_s2')
+
+    args = parser.parse_args()
+    dwell_time = args.dwell_time
+
+    S = pysmurf.client.SmurfControl(
+            epics_root = args.epics_root,
+            cfg_file = args.config_file,
+            setup = args.setup,make_logfile=False
+    )
+
+    S.stream_data_on()
