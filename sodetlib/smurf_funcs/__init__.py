6d8ec653e5abb3a0b826ec727e01dcf338728851
jlashner <jlashner@gmail.com>
2020-10-13 13:49:45 -0400

    Separated optimization functions and smurf operations

--- sodetlib/smurf_funcs/__init__.py
+++ sodetlib/smurf_funcs/__init__.py
@@ -1,1 +1,5 @@
-from sodetlib.smurf_funcs.health_check import health_check
+"""Module containing smurf control functions"""
+
+from sodetlib.smurf_funcs.health_check import *
+from sodetlib.smurf_funcs.optimize_params import *
+from sodetlib.smurf_funcs.smurf_ops import *
