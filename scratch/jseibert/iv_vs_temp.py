c9a0503de2c042d3892dc3e564de9483da00e8d8
Joe <jseibert575@gmail.com>
2021-04-21 16:45:51 -0700

    Saving updates for in iv vs temp code

--- scratch/jseibert/iv_vs_temp.py
+++ scratch/jseibert/iv_vs_temp.py
@@ -15,6 +15,7 @@ import glob
 from sodetlib.det_config  import DetConfig
 import numpy as np
 
+from sodetlib.smurf_funcs import tracking_quality
 import sodetlib.smurf_funcs.optimize_params as op
 import sodetlib.util as su
 from sodetlib.smurf_funcs.det_ops import take_iv
@@ -27,8 +28,8 @@ if __name__=='__main__':
 
     parser.add_argument('--out-file')
 
-    parser.add_argument('--band', type=int, default=2)
-    parser.add_argument('--bias-group',type=int,default=0)
+    #parser.add_argument('--band', type=int,nargs='+', default=[0,1,2,3,4,5,6,7])
+    #parser.add_argument('--bias-group',type=int,nargs='+',default=[0,1,2,3,4,5,6,7,8,9,10,11])
     parser.add_argument('--chans',type=int,nargs='+',default=None)
     parser.add_argument('--ob-volt',type=float,default=19.9)
     parser.add_argument('--step-volt',type=float,default=0.1)
@@ -36,45 +37,61 @@ if __name__=='__main__':
     
     # Initialize pysmurf object
     cfg = DetConfig()
-    cfg.load_config_files(slot=2)
+    cfg.load_config_files(slot=3)
     S = cfg.get_smurf_control()
     
     # Parse args
     args = parser.parse_args()
     
-    band = args.band
-    bias_group = args.bias_group
+    #bands = args.band
+    #bias_group = args.bias_group
+    bands = [0,1,2,3,4,5,6,7]
+    bias_group = [0,1,2,3,4,5,6,7,8,9,10,11]
     ob_volt = args.ob_volt
     step_size = args.step_volt
     good_resp_chans = args.chans
     temp = args.temp
     
+    ucs = [16,18,18,24,18,14,26,26]; dcs = [12,14,14,30,8,30,30,30]
 
     # Set attenuators to optimal values
-    S.set_att_uc(band,8)
-    S.set_att_dc(band,8)
-    
+    #S.set_att_uc(band,8)
+    #S.set_att_dc(band,8)
+
+    tk = {}
+    for band in bands:
+        S.set_att_uc(band,ucs[band])
+        S.set_att_dc(band,dcs[band])
+        S.find_freq(band, tone_power=12, make_plot=True, show_plot=False, save_plot=True, amp_cut=0.05,grad_cut = 0.005)
+        S.setup_notches(band, tone_power=12, new_master_assignment=True, min_offset = 0.05)
+        for _ in range(2):
+            S.run_serial_gradient_descent(band)
+            S.run_serial_eta_scan(band)
+        tk[band] = su.get_tracking_kwargs(S,cfg,band)
+        tk[band]['nsamp'] = 2**18
+        x = tracking_quality(S, cfg, band, show_plots=True,tracking_kwargs=tk[band])
     # Tune resonators
-    S.find_freq(band, tone_power=12, make_plot=True, save_plot=True, show_plot=True)
-    S.setup_notches(band,new_master_assignment=True) # should this be which master assignment?
-    for _ in range(2):
-        S.run_serial_gradient_descent(band)
-        S.run_serial_eta_scan(band)    
-    tk = su.get_tracking_kwargs(S,cfg,band)
-    S.tracking_setup(band,**tk)
+    #S.find_freq(band, tone_power=12, make_plot=True, save_plot=True, show_plot=True)
+    #S.setup_notches(band,new_master_assignment=True) # should this be which master assignment?
     
-    if good_resp_chans is None:
-        good_resp_chans = S.which_on(band)
     
-    for chan in S.which_on(band):
-        if chan in good_resp_chans:
-            continue
-        else:
-            S.channel_off(band,chan)
+    #for _ in range(2):
+        #S.run_serial_gradient_descent(band)
+        #S.run_serial_eta_scan(band)    
+    #tk = su.get_tracking_kwargs(S,cfg,band)
+    #S.tracking_setup(band,**tk)
+    
+    #if good_resp_chans is None:
+       # good_resp_chans = S.which_on(band)
+    
+    #for chan in S.which_on(band):
+        #if chan in good_resp_chans:
+            #continue
+        #else:
+        #S.channel_off(band,chan)
     
     # Take IVs
-    iv_info_fp = take_iv(S,bias_groups=[0],bias_high=19.9,bias_step=step_size,cool_wait=120,high_current_mode=False,overbias_voltage=ob_volt,cool_voltage=8.0)
-    
+    iv_info_fp = take_iv(S,bias_groups=bias_group,bias_high=19.9,bias_step=step_size,cool_wait=120,high_current_mode=False,overbias_voltage=ob_volt,cool_voltage=8.0)
     with open(args.out_file, 'a') as fname:
-        fname.write(f'T = {temp} mK, outdir : {S.output_dir}, tunefile : {S.tune_file} iv_info : {iv_info_fp}\n')
+        fname.write(f'T = {temp} mK, outdir: {S.output_dir}, tunefile: {S.tune_file}, iv_info: {iv_info_fp}\n')
     
