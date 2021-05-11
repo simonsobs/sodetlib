df036d6c4daea0ae41ec9d6abafe0c6c064ba917
dpdutcher <ddutcher@princeton.edu>
2021-05-11 18:40:25 -0400

    formatting

--- sodetlib/smurf_funcs/smurf_ops.py
+++ sodetlib/smurf_funcs/smurf_ops.py
@@ -587,9 +587,7 @@ def uxm_setup(S, cfg, bands):
     S = cfg.get_smurf_control()
 
     plotdir,tunefile = so.uxm_setup(S, cfg, bands)
-
     """
-
 
     print('plotting directory is:')
     print(S.plot_dir)
@@ -610,28 +608,44 @@ def uxm_setup(S, cfg, bands):
         print('band {} uc_att {}'.format(band,S.get_att_uc(band)))
 
         S.amplitude_scale[band] = cfg.dev.bands[band]['drive']
-        print('band {} tone power {}'.format(band,S.amplitude_scale[band] ))
+        print('band {} tone power {}'.format(band, S.amplitude_scale[band]))
 
         print('estimating phase delay')
         S.estimate_phase_delay(band)
         print('setting synthesis scale')
         # hard coding it for the current fw
-        S.set_synthesis_scale(band,1)
+        S.set_synthesis_scale(band, 1)
         print('running find freq')
-        S.find_freq(band,tone_power=cfg.dev.bands[band]['drive'],make_plot=True)
+        S.find_freq(band,tone_power=cfg.dev.bands[band]['drive'], make_plot=True)
         print('running setup notches')
-        S.setup_notches(band,tone_power=cfg.dev.bands[band]['drive'],new_master_assignment=True)
+        S.setup_notches(band,tone_power=cfg.dev.bands[band]['drive'],
+                        new_master_assignment=True)
         print('running serial gradient descent and eta scan')
-        S.run_serial_gradient_descent(band);
-        S.run_serial_eta_scan(band);
+        S.run_serial_gradient_descent(band)
+        S.run_serial_eta_scan(band)
         print('running tracking setup')
-        S.set_feedback_enable(band,1) 
-        S.tracking_setup(band,reset_rate_khz=cfg.dev.bands[band]['flux_ramp_rate_khz'],fraction_full_scale=cfg.dev.bands[band]['frac_pp'], make_plot=False, save_plot=False, show_plot=False, channel=S.which_on(band), nsamp=2**18, lms_freq_hz=None, meas_lms_freq=True,feedback_start_frac=cfg.dev.bands[band]['feedback_start_frac'],feedback_end_frac=cfg.dev.bands[band]['feedback_end_frac'],lms_gain=cfg.dev.bands[band]['lms_gain'])
+        S.set_feedback_enable(band, 1) 
+        S.tracking_setup(
+          band, reset_rate_khz=cfg.dev.bands[band]['flux_ramp_rate_khz'],
+          fraction_full_scale=cfg.dev.bands[band]['frac_pp'], make_plot=False,
+          save_plot=False, show_plot=False, channel=S.which_on(band),
+          nsamp=2**18, lms_freq_hz=None, meas_lms_freq=True,
+          feedback_start_frac=cfg.dev.bands[band]['feedback_start_frac'],
+          feedback_end_frac=cfg.dev.bands[band]['feedback_end_frac'],
+          lms_gain=cfg.dev.bands[band]['lms_gain'],
+        )
         print('checking tracking')
-        S.check_lock(band,reset_rate_khz=cfg.dev.bands[band]['flux_ramp_rate_khz'],fraction_full_scale=cfg.dev.bands[band]['frac_pp'], lms_freq_hz=None, feedback_start_frac=cfg.dev.bands[band]['feedback_start_frac'],feedback_end_frac=cfg.dev.bands[band]['feedback_end_frac'],lms_gain=cfg.dev.bands[band]['lms_gain'])
+        S.check_lock(
+          band, reset_rate_khz=cfg.dev.bands[band]['flux_ramp_rate_khz'],
+          fraction_full_scale=cfg.dev.bands[band]['frac_pp'], lms_freq_hz=None,
+          feedback_start_frac=cfg.dev.bands[band]['feedback_start_frac'],
+          feedback_end_frac=cfg.dev.bands[band]['feedback_end_frac'],
+          lms_gain=cfg.dev.bands[band]['lms_gain'],
+        )
 
     print('taking 20s timestream')
-    S.take_noise_psd(20, nperseg=2**16, save_data=True, make_channel_plot=False, return_noise_params=True)
+    S.take_noise_psd(20, nperseg=2**16, save_data=True, make_channel_plot=False,
+                     return_noise_params=True)
 
     S.save_tune()    
     print('plotting directory is:')
@@ -707,26 +721,41 @@ def uxm_relock(S,cfg,bands,input_tune_file):
         print('band {} uc_att {}'.format(band,S.get_att_uc(band)))
 
         S.amplitude_scale[band] = cfg.dev.bands[band]['drive']
-        print('band {} tone power {}'.format(band,S.amplitude_scale[band] ))
+        print('band {} tone power {}'.format(band, S.amplitude_scale[band]))
 
         print('setting synthesis scale')
         # hard coding it for the current fw
-        S.set_synthesis_scale(band,1)
+        S.set_synthesis_scale(band, 1)
 
         print('running relock')
         S.relock(band,tone_power=cfg.dev.bands[band]['drive'])
         
-        S.run_serial_gradient_descent(band);
-        S.run_serial_eta_scan(band);
+        S.run_serial_gradient_descent(band)
+        S.run_serial_eta_scan(band)
         
         print('running tracking setup')
         S.set_feedback_enable(band,1) 
-        S.tracking_setup(band,reset_rate_khz=cfg.dev.bands[band]['flux_ramp_rate_khz'],fraction_full_scale=cfg.dev.bands[band]['frac_pp'], make_plot=False, save_plot=False, show_plot=False, channel=S.which_on(band), nsamp=2**18, lms_freq_hz=None, meas_lms_freq=True,feedback_start_frac=cfg.dev.bands[band]['feedback_start_frac'],feedback_end_frac=cfg.dev.bands[band]['feedback_end_frac'],lms_gain=cfg.dev.bands[band]['lms_gain'])
+        S.tracking_setup(
+          band,reset_rate_khz=cfg.dev.bands[band]['flux_ramp_rate_khz'],
+          fraction_full_scale=cfg.dev.bands[band]['frac_pp'], make_plot=False,
+          save_plot=False, show_plot=False, channel=S.which_on(band),
+          nsamp=2**18, lms_freq_hz=None, meas_lms_freq=True,
+          feedback_start_frac=cfg.dev.bands[band]['feedback_start_frac'],
+          feedback_end_frac=cfg.dev.bands[band]['feedback_end_frac'],
+          lms_gain=cfg.dev.bands[band]['lms_gain'],
+        )
         print('checking tracking')
-        S.check_lock(band,reset_rate_khz=cfg.dev.bands[band]['flux_ramp_rate_khz'],fraction_full_scale=cfg.dev.bands[band]['frac_pp'], lms_freq_hz=None, feedback_start_frac=cfg.dev.bands[band]['feedback_start_frac'],feedback_end_frac=cfg.dev.bands[band]['feedback_end_frac'],lms_gain=cfg.dev.bands[band]['lms_gain'])
+        S.check_lock(
+          band,reset_rate_khz=cfg.dev.bands[band]['flux_ramp_rate_khz'],
+          fraction_full_scale=cfg.dev.bands[band]['frac_pp'], lms_freq_hz=None,
+          feedback_start_frac=cfg.dev.bands[band]['feedback_start_frac'],
+          feedback_end_frac=cfg.dev.bands[band]['feedback_end_frac'],
+          lms_gain=cfg.dev.bands[band]['lms_gain'],
+        )
 
     print('taking 20s timestream')
-    S.take_noise_psd(20, nperseg=2**16, save_data=True, make_channel_plot=False, return_noise_params=True)
+    S.take_noise_psd(20, nperseg=2**16, save_data=True, make_channel_plot=False,
+                     return_noise_params=True)
     S.save_tune()    
 
 
