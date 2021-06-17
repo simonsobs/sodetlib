360b78a1f7a939f4941f771234049366dddcaff7
simonscryo <simonscryo@gmail.com>
2021-06-17 11:54:10 -0500

    updates to scripts

--- scratch/latrt/stream_data.py
+++ scratch/latrt/stream_data.py
@@ -5,11 +5,13 @@ import argparse
 
 matplotlib.use('agg')
 from sodetlib.det_config import DetConfig
+from sodetlib.smurf_funcs.smurf_ops import take_g3_data, stream_g3_on, stream_g3_off
 
 parser = argparse.ArgumentParser(description='Stream Data for some amount of time')
 parser.add_argument('state', choices=['on', 'off'],
             help='Toggle Data Streaming')
 parser.add_argument('--time', type=int, help='Number of seconds to stream data')
+parser.add_argument('--tag', type=str, help='Tag added to G3 file',default='')
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -30,13 +32,13 @@ if __name__ == '__main__':
     S = cfg.get_smurf_control(dump_configs=True)
 
     if args.state == 'off':
-        S.stream_data_off()
+        stream_g3_off(S)
     else:
         S.pA_per_phi0 = 9e6
         S.R_sh=400e-6
         S.bias_line_resistance=16400.0
 
         if args.time is not None:
-            S.take_stream_data(args.time, return_data=True, register_file=True)
+            take_g3_data(S, args.time, tag=args.tag)
         else:
-            S.stream_data_on()
+            stream_g3_on(S, tag=args.tag)
