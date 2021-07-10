1879bff64a7b737cd3c0a235a960a732dd23902d
jlashner <jlashner@gmail.com>
2021-07-09 17:32:20 -0700

    Change name of health_check to cryo_amp_check

--- scripts/health_check.py
+++ scripts/health_check.py
@@ -4,7 +4,7 @@ import os
 matplotlib.use('Agg')
 
 from sodetlib.det_config import DetConfig
-from sodetlib.smurf_funcs.smurf_ops import health_check
+from sodetlib.smurf_funcs.smurf_ops import cryo_amp_check
 
 if __name__ == '__main__':
     cfg = DetConfig()
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     S = cfg.get_smurf_control(dump_configs=True)
 
     # Put your script calls here
-    health_check(S, cfg)
+    cryo_amp_check(S, cfg)
 
     out_file = os.path.abspath(os.path.expandvars(cfg.dev_file))
     print(f"Writing new device config to {out_file}")
