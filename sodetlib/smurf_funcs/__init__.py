d052c5f14dc91a6c3dca9e0d16e4fa4ebef19723
jlashner <jlashner@gmail.com>
2021-07-09 17:30:17 -0700

    Moved health_check to smurf_ops

--- sodetlib/smurf_funcs/__init__.py
+++ sodetlib/smurf_funcs/__init__.py
@@ -1,5 +1,4 @@
 """Module containing smurf control functions"""
 
-from sodetlib.smurf_funcs.health_check import *
 from sodetlib.smurf_funcs.optimize_params import *
 from sodetlib.smurf_funcs.smurf_ops import *
