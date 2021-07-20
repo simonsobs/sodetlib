2529c536adb8b668a402e1b7df0aaf48fd0c92a2
dpdutcher <ddutcher@princeton.edu>
2021-07-20 19:23:27 -0400

    undo formatting changes

--- sodetlib/smurf_funcs/det_ops.py
+++ sodetlib/smurf_funcs/det_ops.py
@@ -9,8 +9,8 @@ import numpy as np
 import time
 import os
 from sodetlib.util import make_filename
-from sodetlib.analysis import det_analysis
 import sodetlib.smurf_funcs.smurf_ops as so
+from sodetlib.analysis import det_analysis
 
 from pysmurf.client.util.pub import set_action
 
@@ -30,9 +30,8 @@ def get_current_mode(S, bias_group):
     relay = S.get_cryo_card_relays()  # querey twice to ensure update
     if bias_group >= S._n_bias_groups:
         raise ValueError("Biasgroup must be between 0 and {S._n_bias_groups}")
-    r = np.ravel(
-        S._pic_to_bias_group[np.where(S._pic_to_bias_group[:, 1] == bias_group)]
-    )[0]
+    r = np.ravel(S._pic_to_bias_group[np.where(
+        S._pic_to_bias_group[:, 1] == bias_group)])[0]
     return (relay >> r) & 1
 
 
@@ -119,35 +118,29 @@ def take_iv(
     if bias_groups is None:
         bias_groups = np.arange(12)  # SO UFMs have 12 bias groups
     bias_groups = np.array(bias_groups)
-    if overbias_voltage != 0.0:
+    if overbias_voltage != 0.:
         overbias = True
     else:
         overbias = False
     if bias is None:
         # Set actual bias levels
-        bias = np.arange(bias_high, bias_low - bias_step, -bias_step)
+        bias = np.arange(bias_high, bias_low-bias_step, -bias_step)
     # Overbias the TESs to drive them normal
     if overbias:
         if cool_voltage is None:
-            S.overbias_tes_all(
-                bias_groups=bias_groups,
-                overbias_wait=overbias_wait,
-                tes_bias=np.max(bias),
-                cool_wait=cool_wait,
-                high_current_mode=high_current_mode,
-                overbias_voltage=overbias_voltage,
-            )
+            S.overbias_tes_all(bias_groups=bias_groups,
+                               overbias_wait=overbias_wait,
+                               tes_bias=np.max(bias), cool_wait=cool_wait,
+                               high_current_mode=high_current_mode,
+                               overbias_voltage=overbias_voltage)
         else:
-            S.overbias_tes_all(
-                bias_groups=bias_groups,
-                overbias_wait=overbias_wait,
-                tes_bias=cool_voltage,
-                cool_wait=cool_wait,
-                high_current_mode=high_current_mode,
-                overbias_voltage=overbias_voltage,
-            )
-            S.log("Starting to take IV.", S.LOG_USER)
-    S.log("Starting TES bias ramp.", S.LOG_USER)
+            S.overbias_tes_all(bias_groups=bias_groups,
+                               overbias_wait=overbias_wait,
+                               tes_bias=cool_voltage, cool_wait=cool_wait,
+                               high_current_mode=high_current_mode,
+                               overbias_voltage=overbias_voltage)
+            S.log('Starting to take IV.', S.LOG_USER)
+    S.log('Starting TES bias ramp.', S.LOG_USER)
     bias_group_bool = np.zeros((n_bias_groups,))
     # only set things on the bias groups that are on
     bias_group_bool[bias_groups] = 1
@@ -158,7 +151,7 @@ def take_iv(
     sid = so.stream_g3_on(S, make_freq_mask=False)  
     S.log(f'g3 stream id: {sid}')
     for b in bias:
-        S.log(f"Bias at {b:4.3f}")
+        S.log(f'Bias at {b:4.3f}')
         S.set_tes_bias_bipolar_array(b * bias_group_bool)
         time.sleep(wait_time)
     datfile = S.get_data_file_name(as_string=True)
@@ -169,7 +162,7 @@ def take_iv(
     path = os.path.join(S.output_dir, basename + '_iv_bias_all')
     np.save(path, bias)
     # publisher announcement
-    S.pub.register_file(path, "iv_bias", format="npy")
+    S.pub.register_file(path, 'iv_bias', format='npy')
 
     iv_info = {}
     iv_info['plot_dir'] = S.plot_dir
@@ -196,8 +189,8 @@ def take_iv(
     iv_info_fp = os.path.join(S.output_dir, basename + '_iv_info.npy')
 
     np.save(iv_info_fp, iv_info)
-    S.log(f"Writing IV information to {iv_info_fp}.")
-    S.pub.register_file(iv_info_fp, "iv_info", format="npy")
+    S.log(f'Writing IV information to {iv_info_fp}.')
+    S.pub.register_file(iv_info_fp, 'iv_info', format='npy')
 
     if do_analysis:
         timestamp, phase, mask, v_bias = det_analysis.load_from_sid(cfg, iv_info_fp)
@@ -238,16 +231,8 @@ def take_iv(
 
 
 @set_action()
-def take_tickle(
-    S,
-    cfg,
-    bias_groups,
-    tickle_freq=5.0,
-    tickle_voltage=0.005,
-    duration=3.0,
-    high_current=False,
-    silence_pysmurf=True,
-):
+def take_tickle(S, cfg, bias_groups, tickle_freq=5., tickle_voltage=0.005,
+                duration=3., high_current=False, silence_pysmurf=True):
     """
     Takes a tickle measurement on one or more bias groups. If multiple bias
     groups are specified, will play a tickle over each bias group in sequence,
@@ -255,10 +240,6 @@ def take_tickle(
 
     Args
     ----
-    S : 
-        SmurfControl object
-    cfg :
-        DetConfig object
     bias_group : (int, list[int])
         bias group or list of bias groups to tickle.
     tickle_freq : float
@@ -288,14 +269,14 @@ def take_tickle(
         bias_groups = [bias_groups]
 
     if silence_pysmurf:
-        fname = make_filename(S, "take_tickle.log")
+        fname = make_filename(S, 'take_tickle.log')
         S.set_logfile(fname)
 
     init_biases = S.get_tes_bias_bipolar_array()
     bias_array = S.get_tes_bias_bipolar_array()
     bias_groups = np.array(bias_groups)
     start_times = np.zeros_like(bias_groups, dtype=np.float64)
-    stop_times = np.zeros_like(bias_groups, dtype=np.float64)
+    stop_times = np.zeros_like(bias_groups,  dtype=np.float64)
     dat_files = []
 
     if (bias_groups >= 12).any():
@@ -309,10 +290,8 @@ def take_tickle(
         if high_current and (not orig_hc_mode):
             new_bias = orig_bias / S.high_low_current_ratio
             print(f"Setting bias_group {bg} to high current mode.")
-            print(
-                f"Changing bias from {orig_bias} to {new_bias} to preserve "
-                f"dc-current."
-            )
+            print(f"Changing bias from {orig_bias} to {new_bias} to preserve "
+                  f"dc-current.")
             S.set_tes_bias_high_current(bg)
             S.set_tes_bias_bipolar(bg, new_bias)
             bias_array[bg] = new_bias
@@ -330,9 +309,8 @@ def take_tickle(
         S.set_tes_bias_bipolar(bg, init_biases[bg])
 
         if new_bias is not None:
-            print(
-                f"Restoring bg {bg} to low-current mode  and dc bias to " f"{orig_bias}"
-            )
+            print(f"Restoring bg {bg} to low-current mode  and dc bias to "
+                  f"{orig_bias}")
             S.set_tes_bias_low_current(bg)
             S.set_tes_bias_bipolar(bg, orig_bias)
 
@@ -341,18 +319,18 @@ def take_tickle(
     S.set_logfile(None)
 
     summary = {
-        "tickle_freq": tickle_freq,
-        "tone_voltage": tickle_voltage,
-        "high_current": high_current,
-        "bias_array": bias_array,
-        "bias_groups": bias_groups,
-        "start_times": start_times,
-        "stop_times": stop_times,
-        "dat_files": dat_files,
+        'tickle_freq': tickle_freq,
+        'tone_voltage': tickle_voltage,
+        'high_current': high_current,
+        'bias_array': bias_array,
+        'bias_groups': bias_groups,
+        'start_times': start_times,
+        'stop_times': stop_times,
+        'dat_files': dat_files,
     }
-    filename = make_filename(S, "tickle_summary.npy")
+    filename = make_filename(S, 'tickle_summary.npy')
     np.save(filename, summary, allow_pickle=True)
-    S.pub.register_file(filename, "tickle_summary", format="npy")
+    S.pub.register_file(filename, 'tickle_summary', format='npy')
     print(f"Saved tickle summary to {filename}")
     return filename
 
@@ -362,7 +340,7 @@ def bias_detectors_from_sc(S, bias_points_fp, high_current_mode=False):
     """
     Biases detectors using the bias points calculated for various bias
     groups using sodetlib.det_analysis.find_bias_points. Will bias any
-    bias groups that are specified in the file at bias_points_fp.
+    bias groups that are specified in the file at bias_points_fp. 
 
     Args
     ----
@@ -390,12 +368,10 @@ def bias_detectors_from_sc(S, bias_points_fp, high_current_mode=False):
     bias_array = S.get_tes_bias_bipolar_array()
     bias_array[bias_groups] = bias_values
 
-    print(f"Overbiasing and setting TES biases on bias groups {bias_groups}.")
-    S.overbias_tes_all(
-        bias_groups=bias_groups,
-        overbias_voltage=overbias_voltage,
-        tes_bias=overbias_voltage,
-        high_current_mode=high_current_mode,
-    )
+    print(f'Overbiasing and setting TES biases on bias groups {bias_groups}.')
+    S.overbias_tes_all(bias_groups=bias_groups,
+                       overbias_voltage=overbias_voltage,
+                       tes_bias=overbias_voltage,
+                       high_current_mode=high_current_mode)
 
     S.set_tes_bias_bipolar_array(bias_array)
