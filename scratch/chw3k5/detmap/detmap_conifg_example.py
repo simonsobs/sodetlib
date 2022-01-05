e694bfbceee1e4872dd338be02f5aca979f15e9d
chw3k5 <chw3k5@gmail.com>
2022-01-05 15:22:28 -0600

    Reorganization to move from metadata/ to config_files/

--- scratch/chw3k5/detmap/detmap_conifg_example.py
+++ scratch/chw3k5/detmap/detmap_conifg_example.py
@@ -10,21 +10,20 @@ from download_example_data import sample_data_init
 
 # Check to see if the example data is available, if not it downloads it from a GoogleDrive host.
 sample_data_init()
-# get the absolute path for the configuration files
-abs_path_config_files = pathlib.Path(__file__).parent.resolve()
+
 # get the absolute path fo the detector mapping code
-abs_path_detmap = abs_path_config_files.parent.resolve()
+abs_path_detmap = pathlib.Path(__file__).parent.resolve()
 # set the absolute paths for the example and metadata directories.
-abs_path_metadata = os.path.join(abs_path_detmap, 'metadata')
 abs_path_sample_data = os.path.join(abs_path_detmap, 'sample_data')
+# get the absolute path for the configuration files
+abs_path_config_files = os.path.join(abs_path_detmap, 'config_files')
 
 """
 start the example configuration file, this is only a first try, most of this should be determined dynamically.
 """
 north_is_highband = False  # use None for no highband, True for North as the highband, False for South as the highband
-vna_shift_mhz = 10  # in MHz
-waferfile = os.path.join(abs_path_metadata, "copper_map_corrected.csv")
-design_file = os.path.join(abs_path_metadata, "umux_32_map.pkl")
+waferfile = os.path.join(abs_path_config_files, "copper_map_corrected.csv")
+design_file = os.path.join(abs_path_config_files, "umux_32_map.pkl")
 bands = range(8)
 dark_bias_lines = [4, 5, 6, 7, 8, 9, 10, 11]  # If certain sides are covered
 tunefile = os.path.join(abs_path_sample_data, '1632247315_tune.npy')
@@ -33,9 +32,10 @@ tunefile = os.path.join(abs_path_sample_data, '1632247315_tune.npy')
 mux_pos_num_to_mux_band_num_path = os.path.join(abs_path_config_files, 'mux_pos_num_to_mux_band_num.csv')
 
 # cold ramp bath temperature sweep filename
-cold_ramp_file = os.path.join(abs_path_config_files, 'coldloadramp_example.csv')
+cold_ramp_file = os.path.join(abs_path_sample_data, 'coldloadramp_example.csv')
 
-# vna data paths
+# vna data
+vna_shift_mhz = 10.0  # in MHz, applies a shift to the VNA data when determining the smurf band and resonator index
 path_north_side_vna = os.path.join(abs_path_sample_data, 'north_side_vna_farray.csv')
 path_south_side_vna = os.path.join(abs_path_sample_data, 'south_side_vna_farray.csv')
 # below is an intermediate data product with assigned smurf bands
