6424b1595dcbe9fe79e3bc59b2256f6b59b500f9
simonscryo <cryo@smurf-srv15>
2022-01-12 16:14:03 +0000

    ddutcher scratch setup scripts

---
+++ scratch/ddutcher/tes_yield_devel.py
@@ -0,0 +1,273 @@
+# tes_yield.py
+'''
+Code written in Oct 2021 by Yuhan Wang.
+Check TES yield by taking bias tickle (from sodetlib) and IV curves.
+Display quality in biasability, 50% RN target V bias, Psat and Rn.
+'''
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import pysmurf.client
+import argparse
+import numpy as np
+import os
+import time
+import glob
+from sodetlib.det_config  import DetConfig
+import numpy as np
+from scipy.interpolate import interp1d
+import argparse
+import time
+import csv
+from sodetlib.smurf_funcs import det_ops
+from sodetlib.analysis import det_analysis
+from pysmurf.client.util.pub import set_action
+
+
+def tickle_and_iv(
+        S, target_bg, bias_high, bias_low, bias_step, bath_temp, start_time, current_mode
+):
+    overbias_voltage = 18
+    target_bg = np.array(target_bg)
+    save_name = '{}_tes_yield.csv'.format(start_time)
+    tes_yield_data = os.path.join(S.output_dir, save_name)
+    print(f'Saving data to {tes_yield_data}')
+    out_fn = os.path.join(S.output_dir, tes_yield_data) 
+
+    fieldnames = ['bath_temp', 'bias_line', 'band', 'data_path','notes']
+    with open(out_fn, 'w', newline = '') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+    print(f'Taking tickle on bias line all band')
+
+    if current_mode.lower() in ['high, hi']:
+        high_current_mode = True
+        bias_high /= S.high_low_current_ratio
+        bias_low /= S.high_low_current_ratio
+        bias_step /= S.high_low_current_ratio
+    else:
+        high_current_mode = False
+
+    tickle_file = det_ops.take_tickle(
+        S, cfg, target_bg, tickle_freq=5, tickle_voltage=0.005, high_current=True
+    )
+    tsum, tsum_fp = det_analysis.analyze_tickle_data(S, tickle_file, normal_thresh=0.002)
+
+    row = {}
+    row['bath_temp'] = str(bath_temp)
+    row['bias_line'] = str(target_bg)
+    row['band'] = 'all'
+    row['data_path'] = tickle_file
+
+    with open(out_fn, 'a', newline = '') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow(row)
+
+    for bg in target_bg:
+        row = {}
+        row['bath_temp'] = str(bath_temp)
+        row['bias_line'] = bg
+        row['band'] = 'all'
+
+        print(f'Taking IV on bias line {bg}, all smurf bands.')
+
+        iv_data = det_ops.take_iv(
+            S, cfg,
+            bias_groups = [bg], wait_time=0.01, bias_high=bias_high,
+            bias_low=bias_low, bias_step=bias_step,
+            overbias_voltage=12, cool_wait=30, high_current_mode=high_current_mode,
+            make_channel_plots=False, save_plots=True,
+        )
+        dat_file = iv_data.replace('info','analyze')     
+        row['data_path'] = dat_file
+        with open(out_fn, 'a', newline = '') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writerow(row)
+
+    return out_fn
+
+
+@set_action()
+def tes_yield(S, target_bg, out_fn, start_time):
+    data_dict = np.genfromtxt(out_fn, delimiter=",",unpack=True, dtype=None, names=True, encoding=None)
+    
+    data = []
+    for ind in np.arange(1,len(target_bg) + 1):
+        file_path = str(data_dict['data_path'][ind])
+        data.append(file_path)
+    
+    good_chans = 0
+    all_data_IV = dict()
+
+    for ind, bl in enumerate(target_bg):
+        if bl not in all_data_IV.keys():
+            all_data_IV[bl] = dict()
+        now = np.load(data[bl], allow_pickle=True).item()
+        now = now['data']
+        for sb in now.keys():
+            if len(now[sb].keys()) != 0:
+                all_data_IV[bl][sb] = dict()
+            for chan, d in now[sb].items():
+                if (d['R'][-1] < 5e-3):
+                    continue
+                elif len(np.where(d['R'] > 15e-3)[0]) > 0:
+                    continue
+                all_data_IV[bl][sb][chan] = d
+
+    S.pub.register_file(out_fn, "tes_yield", format='.csv')
+
+    common_biases = set()
+    now_bias = set()
+    for bl in all_data_IV.keys():
+        for sb in all_data_IV[bl].keys():
+            if len(all_data_IV[bl][sb].keys()) != 0:
+                first_chan = next(iter(all_data_IV[bl][sb]))
+                now_bias = set(all_data_IV[bl][sb][first_chan]['v_bias'])
+                if len(common_biases) == 0:
+                    common_biases = now_bias
+                else:
+                    common_biases = common_biases.intersection(now_bias)
+    common_biases = np.array(sorted([np.float("%0.3f" % i) for i in common_biases]))
+    common_biases = np.array(common_biases)
+
+    operating_r = dict()
+    for bl in target_bg:
+        operating_r[bl] = dict()
+        for sb in all_data_IV[bl].keys():
+            if len(all_data_IV[bl][sb].keys()) == 0:
+                continue
+            for v in common_biases:
+                if v not in operating_r[bl].keys():
+                    operating_r[bl][v] = []
+                first_chan = next(iter(all_data_IV[bl][sb]))
+                ind = np.where(
+                    (np.abs(all_data_IV[bl][sb][first_chan]['v_bias'] - v)) < 3e-3
+                )[0][0]
+                for chan, d in all_data_IV[bl][sb].items():
+                    operating_r[bl][v].append(d['R'][ind]/d['R_n']) 
+
+    target_vbias_dict = {}
+    RN = []
+    v_bias_all = []
+    for bl in target_bg:
+        percent_rn = 0.5
+        target_v_bias = []
+
+        for sb in all_data_IV[bl].keys():
+            for ch, d in all_data_IV[bl][sb].items():
+                rn = d['R']/d['R_n']
+                cross_idx = np.where(np.logical_and(rn - percent_rn >= 0, np.roll(rn - percent_rn, 1) < 0))[0]
+                if len(cross_idx) == 0:
+                    continue
+                RN.append(d['R_n'])
+                target_v_bias.append(d['v_bias'][cross_idx][0]) 
+                v_bias_all.append(d['v_bias'][cross_idx][0])
+        med_target_v_bias = np.nanmedian(np.array(target_v_bias))
+        target_vbias_dict[bl] = round(med_target_v_bias,1)
+
+    target_vbias_fp = os.path.join(S.output_dir, f"{start_time}_target_vbias.npy")
+    np.save(target_vbias_fp, target_vbias_dict, allow_pickle=True)
+    S.pub.register_file(target_vbias_fp, "tes_yield", format='npy')
+
+    fig, axs = plt.subplots(6, 4,figsize=(25,30), gridspec_kw={'width_ratios': [2, 1,2,1]})
+    for ind, bl in enumerate(target_bg):
+        if np.isnan(target_vbias_dict[bl]):
+            continue
+        count_num = 0
+        for sb in all_data_IV[bl].keys():
+            for ch,d in all_data_IV[bl][sb].items():
+                axs[bl//2,bl%2*2].plot(d['v_bias'], d['R'], alpha=0.6)
+                count_num = count_num + 1
+        axs[bl//2,bl%2*2].set_xlabel('V_bias [V]')
+        axs[bl//2,bl%2*2].set_ylabel('R [Ohm]')
+        axs[bl//2,bl%2*2].grid()
+        axs[bl//2,bl%2*2].axhspan(2.6e-3, 5.8e-3, facecolor='gray', alpha=0.2)
+        axs[bl//2,bl%2*2].axvline(target_vbias_dict[bl], linestyle='--', color='gray')
+        axs[bl//2,bl%2*2].set_title('bl {}, yield {}'.format(bl,count_num))
+        axs[bl//2,bl%2*2].set_ylim([-0.001,0.012])
+
+        h = axs[bl//2,bl%2*2+1].hist(operating_r[bl][target_vbias_dict[bl]], range=(0,1), bins=40)
+        axs[bl//2,bl%2*2+1].axvline(np.median(operating_r[bl][target_vbias_dict[bl]]),linestyle='--', color='gray')
+        axs[bl//2,bl%2*2+1].set_xlabel("percentage Rn")
+        axs[bl//2,bl%2*2+1].set_ylabel("{} TESs total".format(count_num))
+        axs[bl//2,bl%2*2+1].set_title("optimal Vbias {}V for median {}Rn".format(
+            target_vbias_dict[bl],np.round(np.median(operating_r[bl][target_vbias_dict[bl]]),2))
+        )
+
+    save_name = os.path.join(S.plot_dir, f'{start_time}_IV_yield.png')
+    print(f'Saving plot to {save_name}')
+    plt.savefig(save_name)
+
+    S.pub.register_file(save_name, "tes_yield", plot=True)
+
+    fig, axs = plt.subplots(6, 4, figsize=(25,30))
+    for bl in target_bg:
+        count_num = 0
+        Rn = []
+        psat = []
+        for sb in all_data_IV[bl].keys():
+            for ch,d in all_data_IV[bl][sb].items():
+                Rn.append(d['R_n'])
+                psat.append(d['p_sat'].item())
+                count_num += 1
+
+        axs[bl//2,bl%2*2].set_xlabel('P_sat (pW)')
+        axs[bl//2,bl%2*2].set_ylabel('count')
+        axs[bl//2,bl%2*2].grid()
+        axs[bl//2,bl%2*2].hist(psat, range=(0,15), bins=50,histtype= u'step',linewidth=2,color = 'r')
+        axs[bl//2,bl%2*2].axvline(np.median(psat), linestyle='--', color='gray')
+        axs[bl//2,bl%2*2].set_title('bl {}, yield {} median Psat {:.2f} pW'.format(
+            bl,count_num,np.median(psat))
+        )
+        h = axs[bl//2,bl%2*2+1].hist(Rn, range=(0.005,0.01), bins=50, histtype= u'step',linewidth=2,color = 'k')
+        axs[bl//2,bl%2*2+1].axvline(np.median(Rn),linestyle='--', color='gray')
+        axs[bl//2,bl%2*2+1].set_xlabel("Rn (Ohm)")
+        axs[bl//2,bl%2*2+1].set_ylabel('count')
+        axs[bl//2,bl%2*2+1].set_title('bl {}, median Rn {:.4f} Ohm'.format(bl,np.median(Rn)))
+
+    save_name = os.path.join(S.plot_dir, f'{start_time}_IV_psat.png')
+    print(f'Saving plot to {save_name}')
+    plt.savefig(save_name)
+
+    S.pub.register_file(save_name, "tes_yield", plot=True)
+
+    return target_vbias_dict
+
+
+def run(S, cfg, bias_high=20, bias_low=0, bias_step=0.025, bath_temp=100, current_mode='low'):
+    start_time = S.get_timestamp()
+    target_bg = np.arange(12)
+
+    out_fn = tickle_and_iv(
+        S, target_bg, bias_high, bias_low, bias_step, bath_temp, start_time, current_mode)
+    target_vbias = tes_yield(S, target_bg, out_fn, start_time)
+    print(f'Saving data to {out_fn}')
+    return target_vbias
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--temp', type=str,
+                        help="For record-keeping, not controlling,"
+    )
+    parser.add_argument('--bias-high', type=float, default=20)
+    parser.add_argument('--bias-low', type=float, default=0)
+    parser.add_argument('--bias-step', type=float, default=0.025)
+    parser.add_argument('--current-mode', type=str, default='low')
+
+    cfg = DetConfig()
+    args = cfg.parse_args(parser)
+    S = cfg.get_smurf_control()
+
+    S.load_tune(cfg.dev.exp['tunefile'])
+
+    bath_temp = args.temp
+    bias_high = args.bias_high
+    bias_low = args.bias_low
+    bias_step = args.bias_step
+    current_mode = args.current_mode
+
+    run(S, cfg, bias_high=bias_high, bias_low=bias_low,
+        bias_step=bias_step, bath_temp=bath_temp, current_mode=current_mode)
--- scratch/ddutcher/uxm_setup.py
+++ scratch/ddutcher/uxm_setup.py
@@ -1,66 +1,167 @@
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+# uxm_setup.py
+#
+# Use:
+# python3 uxm_setup.py -h
+# to see the available options and required formatting.
 
-import pysmurf.client
-import argparse
+import os, sys
 import numpy as np
-import pickle as pkl
 from scipy import signal
-import os
+import matplotlib.pyplot as plt
+import sodetlib.smurf_funcs.optimize_params as op
+import logging
 
-from sodetlib.det_config import DetConfig
+sys.path.append('/sodetlib/scratch/ddutcher')
+from noise_stack_by_band import noise_stack_by_band
 
-bands = [3]#[0,1,2,3,4,5,6,7]
-slot_num = 3
+logger = logging.getLogger(__name__)
 
-cfg = DetConfig()
-cfg.load_config_files(slot=slot_num)
-S = cfg.get_smurf_control()
+def uxm_setup(S, cfg, bands=None):
+    """
+    Use values in cfg to setup UXM for use.
+    """
+    logger.info(f"plotting directory is:\n{S.plot_dir}")
+    if bands is None:
+        bands = S.config.get("init").get("bands")
 
-print('plotting directory is:')
-print(S.plot_dir)
+    S.all_off()
+    S.set_rtm_arb_waveform_enable(0)
+    S.set_filter_disable(0)
+    S.set_downsample_factor(20)
+    S.set_mode_dc()
 
-S.all_off()
-#S.set_rtm_arb_waveform_enable(0)
-#S.set_filter_disable(0)
-S.set_downsample_factor(20)
-S.set_mode_dc()
+    for band in bands:
+        logger.info(f"setting up band {band}")
 
-for band in bands:
-	print('setting up band {}'.format(band))
+        S.set_att_dc(band, cfg.dev.bands[band]["dc_att"])
+        logger.info("band {} dc_att {}".format(band, S.get_att_dc(band)))
 
-	S.set_att_dc(band,cfg.dev.bands[band]['dc_att'])
-	print('band {} dc_att {}'.format(band,S.get_att_dc(band)))
+        S.set_att_uc(band, cfg.dev.bands[band]["uc_att"])
+        logger.info("band {} uc_att {}".format(band, S.get_att_uc(band)))
 
-	S.set_att_uc(band,cfg.dev.bands[band]['uc_att'])
-	print('band {} uc_att {}'.format(band,S.get_att_uc(band)))
+        S.amplitude_scale[band] = cfg.dev.bands[band]["drive"]
+        logger.info(
+            "band {} tone power {}".format(band, S.amplitude_scale[band])
+        )
 
-	S.amplitude_scale[band] = cfg.dev.bands[band]['drive']
-	print('band {} tone power {}'.format(band,S.amplitude_scale[band] ))
+        # logger.info("estimating phase delay")
+        # try:
+        #     S.estimate_phase_delay(band)
+        # except Exception:
+        #     logger.warning('Estimate phase delay failed due to PV timeout.')
+        logger.info("setting synthesis scale")
+        # hard coding it for the current fw
+        S.set_synthesis_scale(band, 1)
+        logger.info("running find freq")
+        S.find_freq(band, tone_power=cfg.dev.bands[band]["drive"], make_plot=True)
+        logger.info("running setup notches")
+        S.setup_notches(
+            band, tone_power=cfg.dev.bands[band]["drive"], new_master_assignment=True
+        )
+        logger.info("running serial gradient descent and eta scan")
+        S.run_serial_gradient_descent(band)
+        S.run_serial_eta_scan(band)
+        logger.info("running tracking setup")
+        S.set_feedback_enable(band, 1)
+        S.tracking_setup(
+            band,
+            reset_rate_khz=cfg.dev.bands[band]["flux_ramp_rate_khz"],
+            fraction_full_scale=cfg.dev.bands[band]["frac_pp"],
+            make_plot=True,
+            save_plot=True,
+            show_plot=False,
+            channel=S.which_on(band)[::10],
+            nsamp=2 ** 18,
+            lms_freq_hz=cfg.dev.bands[band]["lms_freq_hz"],
+            meas_lms_freq=cfg.dev.bands[band]["meas_lms_freq"],
+            feedback_start_frac=cfg.dev.bands[band]["feedback_start_frac"],
+            feedback_end_frac=cfg.dev.bands[band]["feedback_end_frac"],
+            lms_gain=cfg.dev.bands[band]["lms_gain"],
+        )
 
-	print('estimating phase delay')
-	S.estimate_phase_delay(band)
-	print('setting synthesis scale')
-	# hard coding it for the current fw
-	S.set_synthesis_scale(band,1)
-	print('running find freq')
-	S.find_freq(band,tone_power=cfg.dev.bands[band]['drive'], stop_freq=225, make_plot=True, amp_cut=0.1)
-	print('running setup notches')
-	S.setup_notches(band,tone_power=cfg.dev.bands[band]['drive'],new_master_assignment=True)
-	print('running serial gradient descent and eta scan')
-	S.run_serial_gradient_descent(band);
-	S.run_serial_eta_scan(band);
-	print('running tracking setup')
-	S.set_feedback_enable(band,1) 
-	S.tracking_setup(band,reset_rate_khz=cfg.dev.bands[band]['flux_ramp_rate_khz'],fraction_full_scale=cfg.dev.bands[band]['frac_pp'], make_plot=True, save_plot=True, show_plot=False, channel=S.which_on(band)[::10], nsamp=2**18, lms_freq_hz=None, meas_lms_freq=True,feedback_start_frac=cfg.dev.bands[band]['feedback_start_frac'],feedback_end_frac=cfg.dev.bands[band]['feedback_end_frac'],lms_gain=cfg.dev.bands[band]['lms_gain'])
-	
-	# print('checking tracking')
-	# S.check_lock(band,reset_rate_khz=cfg.dev.bands[band]['flux_ramp_rate_khz'],fraction_full_scale=cfg.dev.bands[band]['frac_pp'], lms_freq_hz=None, feedback_start_frac=cfg.dev.bands[band]['feedback_start_frac'],feedback_end_frac=cfg.dev.bands[band]['feedback_end_frac'],lms_gain=cfg.dev.bands[band]['lms_gain'])
+    S.save_tune()
+    cfg.dev.update_experiment({'tunefile': S.tune_file}, update_file=True)
 
-print('taking 20s timestream')
-S.take_noise_psd(20, nperseg=2**16, save_data=True, make_channel_plot=False, return_noise_params=True)
 
-S.save_tune()    
-print('plotting directory is:')
-print(S.plot_dir)
+if __name__ == "__main__":
+    import argparse
+    from sodetlib.det_config import DetConfig
+    
+    cfg = DetConfig()
+
+    # set up the parser for this Script
+    parser = argparse.ArgumentParser(
+        description="Parser for uxm_setup.py script."
+    )
+    parser.add_argument(
+        "--bands",
+        type=int,
+        default=None,
+        nargs="+",
+        help="The SMuRF bands to target. Will default to the bands "
+        + "listed in the pysmurf configuration file."
+    )
+
+    # optional arguments
+    parser.add_argument(
+        "--stream-time",
+        dest="stream_time",
+        type=float,
+        default=20.0,
+        help="float, optional, default is 20.0. The amount of time to sleep in seconds while "
+        + "streaming SMuRF data for analysis.",
+    )
+    parser.add_argument(
+        "--fmin",
+        dest="fmin",
+        type=float,
+        default=5.0,
+        help="float, optional, default is 5.0. The lower frequency (Hz) bound used "
+        + "when creating a mask of white noise levels Suggested value of 5.0",
+    )
+    parser.add_argument(
+        "--fmax",
+        dest="fmax",
+        type=float,
+        default=50.0,
+        help="float, optional, default is 50.0. The upper frequency (Hz) bound used "
+        + "when creating a mask of white noise levels Suggested value of 50.0",
+    )
+    parser.add_argument(
+        "--detrend",
+        dest="detrend",
+        default="constant",
+        help="str, optional, default is 'constant'. Passed to scipy.signal.welch.",
+    )
+    parser.add_argument(
+        "--loglevel",
+        type=str.upper,
+        default=None,
+        choices=['DEBUG','INFO','WARNING','ERROR','CRITICAL'],
+        help="Set the log level for printed messages. The default is pulled from "
+        +"$LOGLEVEL, defaulting to INFO if not set.",
+    )
+
+    # parse the args for this script
+    args = cfg.parse_args(parser)
+    if args.loglevel is None:
+        args.loglevel = os.environ.get("LOGLEVEL","INFO")
+    numeric_level = getattr(logging, args.loglevel)
+    logging.basicConfig(
+        format="%(levelname)s: %(funcName)s: %(message)s", level=numeric_level
+    )
+    
+    S = cfg.get_smurf_control(dump_configs=True, make_logfile=(numeric_level != 10))
+
+    # power amplifiers
+    op.cryo_amp_check(S, cfg)
+    # run the defs in this file
+    uxm_setup(S=S, cfg=cfg, bands=args.bands)
+    # plot noise histograms
+    noise_stack_by_band(
+        S,
+        stream_time=args.stream_time,
+        fmin=args.fmin,
+        fmax=args.fmax,
+        detrend=args.detrend,
+    )
