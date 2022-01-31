c5255ee9e134534cbf161584ef51b209dfbfc93b
chw3k5 <chw3k5@gmail.com>
2022-01-31 15:52:56 -0600

    setup.py has been modified to include metadata.
    The metadata CSV files in sodetlib/detmap/meta/ are needed for
    as the default input for many of the functions in makemap.py.
    
    In the future this metadata will be automatically chosen from the
    data in this folder based on the name of the detector array for which
    the mapping is being calculated. So this data will be relied on more
    in the future, and will be used to create a simpler user experience.
    
    Over-rides for custom metadata inputs will remain  available for the
    most ambitious and experimental users.

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup, Extension, find_packages
 
 import versioneer
 
-setup (name = 'sodetlib',
-       description='SODETLIB',
-       packages=find_packages(),
-       scripts=['hammers/jackhammer'],
-       version=versioneer.get_version(),
-       cmdclass=versioneer.get_cmdclass())
+setup(name='sodetlib',
+      description='SODETLIB',
+      packages=find_packages(),
+      scripts=['hammers/jackhammer'],
+      package_dir={'detmap': 'sodetlib/detmap'},
+      package_data={'detmap': ['meta/*']},
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass())
