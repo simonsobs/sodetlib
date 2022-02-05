00b149698c52b2f2677d7e6721497beb6f6c1318
Jack Lashner <jlashner@gmail.com>
2022-02-04 19:05:04 -0500

    Requested changes

--- sodetlib/__init__.py
+++ sodetlib/__init__.py
@@ -2,5 +2,17 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
+import os
+from functools import wraps
+try:
+    import epics
+    from pysmurf.client.util.pub import set_action
+except:
+    # Just replace set_action with functool.wraps if we
+    # can't import pysmurf
+    set_action = lambda *args, **kwargs: wraps
+    os.environ['NO_PYSMURF'] = 'true'
+
+
 from sodetlib.util import *
 from sodetlib.stream import *
