9dba7093edfac65fb06b1827fd12233359c93a85
Joe <jseibert575@gmail.com>
2022-02-04 17:19:29 -0800

    Only import functions to operations.__init__

--- sodetlib/operations/__init__.py
+++ sodetlib/operations/__init__.py
@@ -1,3 +1,5 @@
-from sodetlib.operations.bias_steps import *
-from sodetlib.operations.iv import *
-from sodetlib.operations.bias_dets import *
+from sodetlib.operations.bias_steps import take_bias_step, take_bgmap
+from sodetlib.operations.iv import take_iv
+from sodetlib.operations.bias_dets import (
+    bias_to_rfrac_range, bias_to_rfrac
+)
