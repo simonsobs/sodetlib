725ca7a343c5be368e37149ea695d9f9ea46f4a0
simonscryo <simonscryo@gmail.com>
2022-02-05 13:30:51 -0600

    fix typo

--- sodetlib/operations/__init__.py
+++ sodetlib/operations/__init__.py
@@ -1,4 +1,4 @@
-from sodetlib.operations.bias_steps import take_bias_step, take_bgmap
+from sodetlib.operations.bias_steps import take_bias_steps, take_bgmap
 from sodetlib.operations.iv import take_iv
 from sodetlib.operations.bias_dets import (
     bias_to_rfrac_range, bias_to_rfrac
