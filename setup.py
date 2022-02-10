7799dccc752e228acbd3bb91fbf581cbd8f7e37f
Caleb Wheeler <chw3k5@gmail.com>
2022-02-09 18:08:01 -0600

    Update of setup.py to include metadata files.
    Setup.py has been updated so that when python setup.py install is
    used to install sodetlib, the metadate files and used for making
    detector maps are also copied into the egg. These versioned files
    are database elements of for specific and general array
    configurations.

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
+import os
 from setuptools import setup, Extension, find_packages
 
 import versioneer
+
+# local metadata path
+local_metadata = os.path.join('sodetlib', 'detmap', 'meta')
+local_examples_dir = os.path.join('sodetlib', 'detmap', 'example')
+meta_files_all = [os.path.join(local_metadata, f) for f in os.listdir(local_metadata)
+                  if f[-4:].lower() in {'.csv', '.pkl'}]
 
 setup(name='sodetlib',
       description='SODETLIB',
       packages=find_packages(),
       scripts=['hammers/jackhammer'],
-      package_dir={'detmap': 'sodetlib/detmap'},
-      package_data={'detmap': ['meta/*']},
+      data_files=[(local_metadata, meta_files_all),
+                  (local_examples_dir, [os.path.join(local_examples_dir, 'example.yaml')])],
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass())
