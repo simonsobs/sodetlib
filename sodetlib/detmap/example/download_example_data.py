f0d222029d03983ec617c6c62238fa0602221f0c
Caleb Wheeler <chw3k5@gmail.com>
2022-03-02 16:14:31 -0600

    New system for downloading sample data from google drive
    Needs the module gdown. My old snipit of code was no longer working
    for the sample_data download. This new system works well but adds
    another requirement to requirements.txt.

--- sodetlib/detmap/example/download_example_data.py
+++ sodetlib/detmap/example/download_example_data.py
@@ -2,40 +2,19 @@ import os
 import shutil
 import zipfile
 import requests
+import gdown
 
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-    return None
-
-
-def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk:  # filter out keep-alive new chunks
-                f.write(chunk)
-
-
-def download_file_from_google_drive(id, destination):
-    url = "https://docs.google.com/uc?export=download"
-    session = requests.Session()
-    response = session.get(url, params={'id': id}, stream=True)
-    token = get_confirm_token(response)
-    if token:
-        params = {'id': id, 'confirm': token}
-        response = session.get(url, params=params, stream=True)
-    save_response_content(response, destination)
+real_path_this_file = os.path.dirname(os.path.realpath(__file__))
+abs_path_sodetlib, _ = real_path_this_file.rsplit("sodetlib", 1)
+abs_path_detmap = os.path.join(abs_path_sodetlib, "sodetlib", "detmap")
+sample_data_dir = os.path.join(abs_path_detmap, 'sample_data')
+zip_file_id = '1G8eiJ85zVKu53GCzeVdjHEV8cWqrg6eH'
+zipfile_url = f'https://drive.google.com/file/d/{zip_file_id}/view?usp=sharing'
+zipfile_path = os.path.join(abs_path_detmap, 'sample_data.zip')
 
 
 def sample_data_init(del_dir=False):
     # get the absolute path fo the detector mapping code
-    real_path_this_file = os.path.dirname(os.path.realpath(__file__))
-    abs_path_sodetlib, _ = real_path_this_file.rsplit("sodetlib", 1)
-    abs_path_detmap = os.path.join(abs_path_sodetlib, "sodetlib", "detmap")
-    sample_data_dir = os.path.join(abs_path_detmap, 'sample_data')
     if os.path.exists(sample_data_dir):
         if del_dir:
             # Delete the data directory and downloaded again.
@@ -47,16 +26,13 @@ def sample_data_init(del_dir=False):
             return
     # Download the example time stream data.
     print('The Sample Data used for the default data not found, doing a one time download of the sample data data.')
-    zip_file_id = '1G8eiJ85zVKu53GCzeVdjHEV8cWqrg6eH'
-    zipfile_url = f'https://drive.google.com/file/d/{zip_file_id}/view?usp=sharing'
-    zipfile_path = os.path.join(abs_path_detmap, 'sample_data.zip')
     if os.path.exists(zipfile_path):
         # delete an old (failed) zip file if one is present.
         os.remove(zipfile_path)
     # start the download and tell the user what is happening
     print(f'  Beginning file download of at {zipfile_url}')
     print(f'  This is ~350 Mb file so it make take a while depending on your connection speed...')
-    download_file_from_google_drive(id=zip_file_id, destination=zipfile_path)
+    gdown.download(url=f'https://drive.google.com/uc?id={zip_file_id}', output=zipfile_path)
     print('Download Complete.\n')
 
     # unpack the zip file
@@ -73,3 +49,4 @@ def sample_data_init(del_dir=False):
 
 if __name__ == "__main__":
     sample_data_init(del_dir=True)
+
