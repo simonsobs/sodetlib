b62bb0695f6cd56eccc2680f4564ec7902699fdb
Joe <jseibert575@gmail.com>
2022-03-30 15:07:21 -0700

    Changes to bias step function

--- sodetlib/__init__.py
+++ sodetlib/__init__.py
@@ -16,17 +16,7 @@ except Exception as e:
     os.environ['NO_PYSMURF'] = 'true'
 
 
-from sodetlib.util import (
-    make_filename, pub_ocs_log, load_bgmap, map_band_chans,
-    get_metadata, get_tracking_kwargs, get_psd, get_asd, Registers,
-    get_current_mode_array, set_current_mode, validate_and_save,
-    set_session_data
-)
-
-from sodetlib.stream import (
-    get_session_files, load_session_status, load_session,
-    take_g3_data, stream_g3_on, stream_g3_off
-)
-
+from sodetlib.util import *
+from sodetlib.stream import *
 from sodetlib import noise
 
