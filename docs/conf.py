211a3fd94f687aa5a4685e07c9aec37548775816
Jack Lashner <jlashner@gmail.com>
2022-06-10 15:05:33 -0400

    sotodlib -> sotodlib.core in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,8 @@ autodoc_mock_imports = [
     'sotodlib',
     'sotodlib.io',
     'sotodlib.tod_ops.fft_ops',
-    'sotodlib', 'sotodlib.tod_ops',
+    'sotodlib.tod_ops',
+    'sotodlib.core',
     'tqdm.auto'
 ]
 
