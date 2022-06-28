05695786ba53b045a48c4d92fbc88ba0e0557ece
dpdutcher <ddutcher@princeton.edu>
2022-06-28 19:08:05 -0400

    catch error if All-NaN slice encountered

--- sodetlib/operations/iv.py
+++ sodetlib/operations/iv.py
@@ -335,7 +335,10 @@ def analyze_iv(iva, psat_level=0.9, save=False, update_cfg=False):
         dd_resp_abs = np.abs(dd_resp)
 
         # Find index of superconducting branch
-        sc_idx = np.nanargmax(dd_resp_abs) + 1
+        try:
+            sc_idx = np.nanargmax(dd_resp_abs) + 1
+        except ValueError:
+            continue
         if sc_idx == 1:
             continue
         iva.idxs[i, 0] = sc_idx
@@ -343,7 +346,10 @@ def analyze_iv(iva, psat_level=0.9, save=False, update_cfg=False):
         # Find index of normal branch by finding the min index after
         # sc branch. (Skips a few indices after sc branch to avoid possible
         # phase skipping)
-        nb_idx = sc_idx + 1 + np.nanargmin(iva.resp[i, sc_idx+1:])
+        try:
+            nb_idx = sc_idx + 1 + np.nanargmin(iva.resp[i, sc_idx+1:])
+        except ValueError:
+            continue
         iva.idxs[i, 1] = nb_idx
         nb_fit_idx = (iva.nbiases + nb_idx) // 2
 
