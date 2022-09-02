3e9e9f2338595f54050c4813445c8cbf13b71c9c
Jack Lashner <jlashner@gmail.com>
2022-09-02 10:56:08 -0400

    small change to download script

--- hammers/load_pcie_fw/download_fw.sh
+++ hammers/load_pcie_fw/download_fw.sh
@@ -1,4 +1,4 @@
-#/usr/bin/bash
+#!/bin/bash
 
 cd fw_files
 wget https://github.com/slaclab/smurf-pcie/releases/download/v2.3.0/SmurfKcu1500RssiOffload10GbE-0x02030000-20200512134423-ruckman-7aa0ea1_primary.mcs
